15e9b795e191fcbb0ab2f954841fb9afe91b0639
Suresh <katikalasureshkumar@gmail.com>
2019-07-19 17:15:38 +0530

    Added the tabbed view with react

--- src/App.js
+++ src/App.js
@@ -12,11 +12,31 @@ class App extends Component {
     this.state = {
       response: [],
       endpoint: "http://localhost:3000",
-      fileData: ""
+      editorData: "",
+      currentEditorFileName: ""
     };
     this.showTabs = this.showTabs.bind(this);
   }
 
+  updateEditorData(newFilePath) {
+    console.log(newFilePath);
+    this.setState(prevState => ({
+      response: prevState.response.map(
+        el => el.filepath === this.state.currentEditorFileName ? { ...el, filedata: this.state.editorData } : el
+      ),
+      currentEditorFileName: newFilePath,
+      editorData: (prevState.response.filter(el => {
+        if (el.filepath === newFilePath) {
+          return el;
+        }
+      }))[0].filedata
+    }))
+
+    // this.setState({currentEditorFileName : newFilePath})
+    // this.getFileFromApi(file.filepath)
+    //   .then(data=>this.setState({editorData: data.response}));
+
+  }
   showTabs() {
     var divStyle = {
       "backgroundColor": "#f1f1f1",
@@ -26,11 +46,11 @@ class App extends Component {
       "textAlign": "center",
       // "lineHeight": "75px",
       "fontSize": "12px",
-      "padding" : "10px"
+      "padding": "10px"
     }
     let divsArray = [];
-    this.state.response.map(message => {
-      divsArray.push(<div style={divStyle} onClick={() => { console.log(message); this.getFileFromApi(message).then(data=>this.setState({fileData: data.response}))}}>{message.split('/')[message.split('/').length - 1]}</div>);
+    this.state.response.map(file => {
+      divsArray.push(<div style={divStyle} onClick={() => this.updateEditorData(file.filepath)}>{file.filepath.split('/')[file.filepath.split('/').length - 1]}</div>);
     })
     return divsArray;
   }
@@ -39,7 +59,6 @@ class App extends Component {
     let postObject = { "file": file };
     const response = await fetch('http://ec2-18-219-87-48.us-east-2.compute.amazonaws.com:3000/getFile', {
       method: 'POST',
-      //      mode: 'no-cors',
       body: JSON.stringify(postObject), // string or object
       headers: {
         "Accept": "application/json",
@@ -55,7 +74,8 @@ class App extends Component {
 
   componentDidMount() {
     // const { endpoint } = this.state;
-    const socket = socketIOClient("http://127.0.0.1:3000");
+    // const socket = socketIOClient("http://127.0.0.1:3000");
+    const socket = socketIOClient('ec2-18-219-87-48.us-east-2.compute.amazonaws.com:3000')
 
 
     socket.on('connect', () => {
@@ -64,28 +84,44 @@ class App extends Component {
 
     socket.on("OpenedFiles", (from, message) => {
       console.log(from, message);
-      // Element not in the array
-      if (this.state.response.indexOf(message) < 0) {
-        var joined = this.state.response.concat(message);
+
+      // Tells is file is already displaying in editor
+      if (this.state.response.map(function (e) { return e.filepath; }).indexOf(message) < 0) {
+        let file = {
+          "filepath": message
+        }
+        var joined = this.state.response.concat(file);
         this.setState({ response: joined });
-
-        // this.setState({ response: message })
+        if (this.state.currentEditorFileName !== "") {
+          this.setState(prevState => ({
+            response: prevState.response.map(
+              el => {
+                console.log("In changing the first file block")
+                return el.filepath === this.state.currentEditorFileName ? { ...el, filedata: this.state.editorData } : el
+              }
+            )
+          }))
+        }
+        this.setState({ currentEditorFileName: message })
+        // this.setState({currentEditorFileName : message})
+        this.getFileFromApi(message).then(data => {
+          console.log(data);
+          // let position = this.state.response.map(function(e) { return e.filepath; }).indexOf(filepath);
+          this.setState({ editorData: data.response });
+          this.setState(prevState => ({
+            response: prevState.response.map(
+              el => el.filepath === message ? { ...el, filedata: data.response } : el
+            )
+          }))
+        });
       }
-      this.getFileFromApi(message).then(data => {
-        console.log(data);
-        // editor.setValue(data.response)
-        this.setState({fileData: data.response});
-      });
-
-
     });
-
   }
   handleChange = (event) => {
-    this.setState({ fileData: event.target.value });
+    this.setState({ editorData: event.target.value });
   }
   render() {
-    console.log(this.state.response);
+    console.log(this.state);
     var flexStyle = {
       display: "flex",
       "justifyContent": "flex-start",
@@ -112,7 +148,7 @@ class App extends Component {
         </div>
         <textarea style={textAreaStyle} name="body"
           onChange={this.handleChange}
-          value={this.state.fileData} />
+          value={this.state.editorData} />
       </div>
     )
   }
