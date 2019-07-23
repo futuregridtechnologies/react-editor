e5791da09030b1c86a0a22af02dae9ea529d83ea
Suresh <katikalasureshkumar@gmail.com>
2019-07-23 16:39:20 +0530

    Added commit and save buttons but didnt have the functionality

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import socketIOClient from "socket.io-client/dist/socket.io";
+import Button from 'react-bootstrap/Button';
+import { ButtonToolbar } from 'react-bootstrap';
+import ReactTextareaAutocomplete from "@webscopeio/react-textarea-autocomplete";
+var getCaretCoordinates = require('textarea-caret-position');
+
 
 /* eslint-disable */
-
-
 class App extends Component {
   constructor() {
     super();
@@ -13,9 +16,11 @@ class App extends Component {
       response: [],
       endpoint: "http://localhost:3000",
       editorData: "",
-      currentEditorFileName: ""
+      currentEditorFileName: "",
+      allFiles: []
     };
     this.showTabs = this.showTabs.bind(this);
+
   }
 
   updateEditorData(newFilePath) {
@@ -72,10 +77,37 @@ class App extends Component {
     return myJson;
   }
 
+  getAllFiles = async (file) => {
+    // let postObject = { "file": file };
+    const response = await fetch('http://ec2-18-219-87-48.us-east-2.compute.amazonaws.com:3000/loadfiles', {
+      method: 'GET',
+      // body: JSON.stringify(postObject), // string or object
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Origin': '*',
+      }
+    });
+    const myJson = await response.json(); //extract JSON from the http response
+    console.log(myJson);
+    // this.setState({ allFiles: myJson })
+    this.extractFiles(myJson);
+    // return myJson;
+  }
+
   componentDidMount() {
+        // document.querySelector('textarea').addEventListener('input', function () {
+          document.getElementById('editor').addEventListener('input', function () {
+
+            var coordinates = getCaretCoordinates(this, this.selectionEnd);
+            console.log(coordinates)
+            // console.log(coordinates.top);
+            // console.log(coordinates.left);
+          })
     // const { endpoint } = this.state;
-    // const socket = socketIOClient("http://127.0.0.1:3000");
-    const socket = socketIOClient('ec2-18-219-87-48.us-east-2.compute.amazonaws.com:3000')
+    const socket = socketIOClient("http://127.0.0.1:3000");
+    // const socket = socketIOClient('ec2-18-219-87-48.us-east-2.compute.amazonaws.com:3000')
 
 
     socket.on('connect', () => {
@@ -103,7 +135,6 @@ class App extends Component {
           }))
         }
         this.setState({ currentEditorFileName: message })
-        // this.setState({currentEditorFileName : message})
         this.getFileFromApi(message).then(data => {
           console.log(data);
           // let position = this.state.response.map(function(e) { return e.filepath; }).indexOf(filepath);
@@ -117,9 +148,41 @@ class App extends Component {
       }
     });
   }
+
+  componentWillMount() {
+    this.getAllFiles();
+  }
+
+  extractFiles = (files) => {
+    console.log(files)
+    var onlyfiles = [];
+    this.setState({ allFiles: this.getFiles(files, onlyfiles)});
+  }
+
+  getFiles = (files, onlyfiles) => {
+    files.forEach((file) => {
+      if (file.data == undefined) {
+        onlyfiles.push({
+          value: file.value,
+          id: file.id
+        });
+      } else {
+        this.getFiles(file.data, onlyfiles);
+      }
+    });
+      console.log(onlyfiles)
+    return onlyfiles;
+  }
+
   handleChange = (event) => {
+    let wordArray = event.target.value.split("");
+    let word = "";
+    if (wordArray[wordArray.length - 1] == '@') {
+      console.log("@ detected");
+    }
     this.setState({ editorData: event.target.value });
   }
+
   render() {
     console.log(this.state);
     var flexStyle = {
@@ -131,7 +194,7 @@ class App extends Component {
     let textAreaStyle = {
       margin: "0px",
       position: "absolute",
-      top: "50px",
+      top: "200px",
       bottom: "0px",
       left: "0px",
       right: "0px",
@@ -139,14 +202,28 @@ class App extends Component {
       backgroundColor: "#000000",
       color: 'white'
     }
-    let value = "File data comes here";
+    let commitButtonContainer = {
+      // position: "absolute",
+      position: 'relative',
+      right: '10px',
+      // top: '70px',
+      color: 'white',
+      // zIndex: 1
+    }
+
     return (
       <div>
         <div style={flexStyle} className="flex-container">
           {this.showTabs()}
-
+          {/* {this.extractFiles(this.state.allFiles)} */}
         </div>
-        <textarea style={textAreaStyle} name="body"
+        <div style={commitButtonContainer}>
+          <ButtonToolbar>
+            <Button variant="success" onClick={() => { console.log("Commit Button Clicked") }}>Commit</Button>
+            <Button variant="info" onClick={() => { console.log("Save button clicked") }}>Save</Button>
+          </ButtonToolbar>
+        </div>
+        <textarea style={textAreaStyle} name="body" id="editor"
           onChange={this.handleChange}
           value={this.state.editorData} />
       </div>
@@ -155,3 +232,37 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div className="controls">
+            <button onClick={this.resetCaretPosition}>Reset caret position</button>
+            <button onClick={this.printCurrentCaretPosition}>Print current caret position to the console</button>
+        </div> */}
+{/* <ReactTextareaAutocomplete
+          className="my-textarea"
+          loadingComponent={() => <span>Loading</span>}
+          trigger = {{
+            ":": {
+              dataProvider: token => {
+                return [
+                  { name: "smile", char: "🙂" },
+                  { name: "heart", char: "❤️" }
+                ];
+              },
+              component: Item,
+              output: (item, trigger) => item.char
+            }
+          }}          
+          ref={(rta) => { this.rta = rta; } }
+          onCaretPositionChange={this.onCaretPositionChange}
+        /> */}
