3ed76ab8b56690f3f111c2932df20d5db0b7c884
Praveen Bisht <prvnbist@gmail.com>
2019-08-20 18:31:51 +0530

    Added: Tab component with add/remove tab functionality

--- src/sections/Main.jsx
+++ src/sections/Main.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import React from 'react'
+
+import { Tabs, TabList, Tab, TabPanels, TabPanel } from '@reach/tabs'
+
+import { AddIcon, CloseIcon } from '../assets/Icons'
 
 const Main = () => {
-  return <main id="main">Main</main>;
-};
+	const [tabs, setTabs] = React.useState([
+		{
+			name: 'One',
+			content: 'Tab1',
+		},
+		{
+			name: 'Two',
+			content: 'Tab2',
+		},
+		{
+			name: 'Three',
+			content: 'Tab3',
+		},
+		{
+			name: 'Four',
+			content: 'Tab4',
+		},
+	])
+	const removeTab = index =>
+		setTabs([...tabs.filter((_, tabIndex) => tabIndex !== index)])
+	const addTab = () => setTabs([...tabs, { name: 'Five', content: 'Tab5' }])
+	return (
+		<main id="main">
+			<Tabs>
+				<TabList>
+					{tabs.map((tab, index) => (
+						<Tab key={index}>
+							<span>{tab.name}</span>
+							<span onClick={() => removeTab(index)}>
+								{CloseIcon}
+							</span>
+						</Tab>
+					))}
+					<Tab onClick={() => addTab()}>{AddIcon}</Tab>
+				</TabList>
 
-export default Main;
+				<TabPanels>
+					{tabs.map((tab, index) => (
+						<TabPanel key={index}>{tab.content}</TabPanel>
+					))}
+				</TabPanels>
+			</Tabs>
+		</main>
+	)
+}
+
+export default Main
