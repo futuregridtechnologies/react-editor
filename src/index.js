04b6154559c31a1f4e7f160fb946e3cf8645417c
Praveen Bisht <prvnbist@gmail.com>
2019-08-20 15:30:44 +0530

    Added: Apollo GraphQL

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-import App from "./App";
+// Apollo Client Imports
+import { ApolloProvider } from '@apollo/react-hooks'
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+import { ApolloLink } from 'apollo-link'
 
-import "./styles/index.scss";
+// Components
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Styles
+import './styles/index.scss'
+
+const client = new ApolloClient({
+	link: ApolloLink.from([
+		new HttpLink({
+			uri: process.env.REACT_APP_GRAPHQL_URI,
+		}),
+	]),
+	cache: new InMemoryCache(),
+})
+
+ReactDOM.render(
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>,
+	document.getElementById('root')
+)
