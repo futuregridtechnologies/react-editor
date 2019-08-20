7c98f7f8007d76ae0f1686084aac86aac67becb2
Praveen Bisht <prvnbist@gmail.com>
2019-08-20 15:22:19 +0530

    Added: Collapse Navbar Logic & Styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import React from "react";
+import React from 'react'
 
-import Header from "./sections/Header";
-import Navbar from "./sections/Navbar";
-import Main from "./sections/Main";
-import Sidebar from "./sections/Sidebar";
+import Header from './sections/Header'
+import Navbar from './sections/Navbar'
+import Main from './sections/Main'
+import Sidebar from './sections/Sidebar'
 
 const App = () => {
-  return (
-    <div id="wrapper">
-      <Header title="File Editor" />
-      <Navbar />
-      <Main />
-      <Sidebar />
-    </div>
-  );
-};
+	const [isNavbarCollapsed, setNavbarState] = React.useState(false)
+	return (
+		<div
+			id="wrapper"
+			className={`${isNavbarCollapsed ? 'navbar__collapsed' : ''}`}
+		>
+			<Header title="File Editor" />
+			<Navbar
+				isNavbarCollapsed={isNavbarCollapsed}
+				setNavbarState={setNavbarState}
+			/>
+			<Main />
+			<Sidebar />
+		</div>
+	)
+}
 
-export default App;
+export default App
--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
 
-const Navbar = () => {
-	return <aside id="navbar">Navbar</aside>
+import {
+	CollapseLeftIcon,
+	CaretLeftIcon,
+	CaretRightIcon,
+	ExpandRightIcon,
+} from '../assets/Icons'
+
+const Navbar = ({ isNavbarCollapsed, setNavbarState }) => {
+	return (
+		<aside id="navbar">
+			<div id="navbar__header">
+				<div id="navbar__header__nav">
+					<button>{CaretLeftIcon}</button>
+					<button>{CaretRightIcon}</button>
+				</div>
+				<div
+					id="navbar__header__collapse"
+					onClick={() => setNavbarState(!isNavbarCollapsed)}
+				>
+					{isNavbarCollapsed ? ExpandRightIcon : CollapseLeftIcon}
+				</div>
+			</div>
+		</aside>
+	)
 }
 
 export default Navbar
