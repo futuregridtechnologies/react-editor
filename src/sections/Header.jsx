b638a5fb61233f285e7b0132e740c97c3597665b
Praveen Bisht <prvnbist@gmail.com>
2019-08-20 17:13:29 +0530

    Added: Header content & styles

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React from 'react'
+
+import { MinimizeIcon, CloseIcon } from '../assets/Icons'
 
 const Header = ({ title }) => {
-  return <header id="header">{title}</header>;
-};
+	return (
+		<header id="header">
+			<div id="header__title">{title}</div>
+			<div id="header__actions">
+				<button>{MinimizeIcon}</button>
+				<button>{CloseIcon}</button>
+			</div>
+		</header>
+	)
+}
 
-export default Header;
+export default Header
