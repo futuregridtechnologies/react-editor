fe15db1fc0325b852d9f813308cb6215947dbb15
Praveen Bisht <prvnbist@gmail.com>
2019-10-15 15:55:53 +0530

    Optimize: Commit History

--- src/components/Editor/History.jsx
+++ src/components/Editor/History.jsx
@@ -1,59 +1,47 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useQuery } from '@apollo/react-hooks'
 
-import GET_COMMIT_FETCH from '../../queries/getCommit'
-
-import fetchCall from '../../utils/fetchCall'
+import GET_COMMITS from '../../queries/getCommits'
 
 const History = props => {
-	const [commits, setCommits] = React.useState([])
-	React.useEffect(() => {
-		if (props.commits.length > 0) {
-			let bodies = []
-			for (let commit of props.commits) {
-				bodies.push(
-					JSON.stringify({
-						query: GET_COMMIT_FETCH,
-						variables: {
-							path: props.path
-								.split('/')
-								.slice(0, 6)
-								.join('/'),
-							id: commit,
-						},
-					})
-				)
-			}
-			bodies.forEach(async body => {
-				await fetchCall(body).then(({ data }) =>
-					setCommits(commits => [...commits, data.getCommit])
-				)
-			})
-		}
-	}, [props.commits, props.path])
+	const { loading, data: commits } = useQuery(GET_COMMITS, {
+		variables: {
+			path: props.path
+				.split('/')
+				.slice(0, 6)
+				.join('/'),
+			commits: props.commits,
+		},
+	})
+	if (loading)
+		return (
+			<div id="history__panel">
+				<header>
+					<h3>History</h3>
+				</header>
+				<main>Loading</main>
+			</div>
+		)
 	return (
 		<div id="history__panel">
 			<header>
 				<h3>History</h3>
 			</header>
 			<main>
-				{commits
-					.sort(
-						(a, b) => b.committer.timestamp - a.committer.timestamp
-					)
-					.map((commit, index) => (
-						<div className="commit" key={index}>
-							<span>{commit.message}</span>
-							<span>
-								{new Intl.DateTimeFormat('en-US', {
-									month: 'short',
-									day: 'numeric',
-									hour: 'numeric',
-									minute: 'numeric',
-								}).format(commit.committer.timestamp * 1000)}
-							</span>
-						</div>
-					))}
+				{commits.getCommits.map((commit, index) => (
+					<div className="commit" key={index}>
+						<span>{commit.message}</span>
+						<span>
+							{new Intl.DateTimeFormat('en-US', {
+								month: 'short',
+								day: 'numeric',
+								hour: 'numeric',
+								minute: 'numeric',
+							}).format(commit.committer.timestamp * 1000)}
+						</span>
+					</div>
+				))}
 			</main>
 		</div>
 	)
